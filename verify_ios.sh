f3c00b19490e7a856500acde33bb148bb331f991
Anthony Sheetz <asheetz2000@gmail.com>
2026-05-09 11:35:29 -0400

    Force x86_64 for iOS simulator build

--- verify_ios.sh
+++ verify_ios.sh
@@ -55,7 +55,7 @@ if [[ -z "$DEVICE_ID" ]]; then
 fi
 
 echo "   Using simulator: $SIMULATOR_NAME ($DEVICE_ID)"
-flutter build ios --simulator --debug
+arch -x86_64 flutter build ios --simulator --debug
 flutter install -d "$DEVICE_ID"
 
 # --- 4. Run Maestro E2E ---
